ff89b724987191ff8506d22b16cbbe3c5ab9e526
Joao Junior <jcajcefet@yahoo.com.br>
2019-03-19 23:25:04 +0100

    Add model to Letter

--- code/postwoman/models.py
+++ code/postwoman/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.utils.timezone import now
 
 
 class PostWoman(models.Model):
     name = models.CharField(max_length=200, unique=True)
     max_distance = models.FloatField(default=10)
+
+
+class Letter(models.Model):
+    latitude = models.FloatField(default=0)
+    longitute = models.FloatField(default=0)
+    date = models.DateField(default=now)
+    delivered = models.BooleanField(default=False)
+    postman = models.ForeignKey(PostWoman, on_delete=models.CASCADE)
