330dcf6747e917468ac3409aab0e1bd398e85160
Joao Junior <jcajcefet@yahoo.com.br>
2019-03-20 00:03:57 +0100

    Add a crud to Letter

--- code/postwoman/views.py
+++ code/postwoman/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, permissions
 
-from postwoman.serializers import PostWomanSerializer
-from postwoman.models import PostWoman
+from postwoman.serializers import PostWomanSerializer, LetterSerializer
+from postwoman.models import PostWoman, Letter
 
 
 class BaseViewSet(viewsets.ModelViewSet):
@@ -11,3 +11,8 @@ class BaseViewSet(viewsets.ModelViewSet):
 class PostWomanViewSet(BaseViewSet):
     queryset = PostWoman.objects.all().order_by('name')
     serializer_class = PostWomanSerializer
+
+
+class LetterViewSet(BaseViewSet):
+    queryset = Letter.objects.all().order_by('-date', 'latitude', 'longitude')
+    serializer_class = LetterSerializer
